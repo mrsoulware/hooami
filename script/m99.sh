65e522ba6b87c949af3e63be097e20e77d176bb9
mrsoulware <sircoon@gmail.com>
2023-07-05 10:59:45 +0000

    전체 보기 출력 형식 개선

--- script/m99.sh
+++ script/m99.sh
@@ -23,21 +23,34 @@ recurcall() {
 	local nexturl
 	local depth
 	local nextdepth
+	local output
+	local linecnt
+	local lineno
 
 	url=$1
 	depth=$2
 	nextdepth=$(expr $depth + 1)
 
-	curl -s -H "X-aws-ec2-metadata-token: $TOKEN" $url |
+	
+	output=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" $url)
+	linecnt=$(echo "$output" | wc -l)
+	lineno=0
+
+	echo "$output" |
 	while read -r line || [[ -n "$line" ]]
 	do
+		((lineno++))
 		i=1
 		while [ "$i" -lt "$2" ]
 		do
   			echo -n "│   "
 			((i++))
 		done
-		echo -n "├── "
+		if [ "$lineno" == "$linecnt" ]; then
+			echo -n "└── "
+		else
+			echo -n "├── "
+		fi
 
 		index=$((${#line}-1))
 		lastch=${line:$index:1}
